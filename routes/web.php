1ed0633db2d9c4cff303c60148a394d36b4a2a5d
DorisYang <doris@DorisYangdeMacBook-Pro.local>
2018-09-08 19:10:19 +0800

    update test array_multisort

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
+Route::get('/test', 'StaticPagesController@test');
